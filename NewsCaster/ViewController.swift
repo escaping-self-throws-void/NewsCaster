1a73a2fe82c29d2b50942b63cd9d349b4ed1bea2
Paul Matar <paul_matar@icloud.com>
2023-06-08 21:55:04 +0300

    Initial Commit

---
+++ NewsCaster/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsCaster
+//
+//  Created by Paul Matar on 08/06/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
